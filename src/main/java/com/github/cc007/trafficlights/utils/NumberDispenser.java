bb277cbdce13c5ac11e374f34597f678e590eba9
CC007 <Coolcat_the_best2@hotmail.com>
2017-03-31 17:19:19 +0200

    stack bug

--- src/main/java/com/github/cc007/trafficlights/utils/NumberDispenser.java
+++ src/main/java/com/github/cc007/trafficlights/utils/NumberDispenser.java
@@ -13,69 +13,70 @@
  * See the GNU General Public License for more details.
  * See the documentation of Green Light District for further information.
  *------------------------------------------------------------------------*/
-
 package com.github.cc007.trafficlights.utils;
 
 import com.github.cc007.trafficlights.xml.*;
 import java.io.IOException;
 import java.util.Stack;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 // A very simple number dispenser for ID's. It sucks. It also works.
+public class NumberDispenser implements XMLSerializable {
 
-public class NumberDispenser implements XMLSerializable
-{ 	Stack stack;
-  	int counter;
-	String parentName="model";
-  
-  	public NumberDispenser ()
-  	{ 	stack=new Stack();
-    		counter=0;
-  	}
-  
-  	public int get ()
-  	{ 	if (stack.isEmpty()) {
-        return counter++;
-      } else {
-        return ((Integer)(stack.pop())).intValue();
-      }
-	}
-  
-  	public void giveBack (int number)
-  	{ 	if (number == counter - 1) {
-        counter--;
-      } else if (number < counter && ! stack.contains(new Integer (number))) {
-          stack.push (new Integer (number));
-      }
-  	}
-	
-@Override
-	public void load (XMLElement myElement,XMLLoader loader) throws XMLTreeException,IOException,XMLInvalidInputException
-	{ 	stack=new Stack();
-		stack.addAll((ArrayList)(XMLArray.loadArray(this,loader)));
-		counter=myElement.getAttribute("counter").getIntValue();
-	}
+    Stack stack;
+    int counter;
+    String parentName = "model";
 
-@Override
-	public XMLElement saveSelf () throws XMLCannotSaveException
-	{ 	XMLElement result=new XMLElement("dispenser");
-		result.addAttribute(new XMLAttribute ("counter",counter));
-	  	return result;
-	}
-  
-@Override
-	public void saveChilds (XMLSaver saver) throws XMLTreeException,IOException,XMLCannotSaveException
-	{ 	XMLArray.saveArray(stack,this,saver,"stack");
-	}
+    public NumberDispenser() {
+        stack = new Stack();
+        counter = 0;
+    }
 
-@Override
-	public String getXMLName ()
-	{ 	return parentName+".dispenser";
-	}
-	
-@Override
-	public void setParentName (String parentName)
-	{	this.parentName=parentName; 
-	}
-	
+    public int get() {
+        if (stack.isEmpty()) {
+            return counter++;
+        } else {
+            return ((Integer) (stack.pop())).intValue();
+        }
+    }
+
+    public void giveBack(int number) {
+        if (number == counter - 1) {
+            counter--;
+        } else if (number < counter && !stack.contains(new Integer(number))) {
+            stack.push(new Integer(number));
+        }
+    }
+
+    @Override
+    public void load(XMLElement myElement, XMLLoader loader) throws XMLTreeException, IOException, XMLInvalidInputException {
+        stack = new Stack();
+        stack.addAll((ArrayList) (XMLArray.loadArray(this, loader)));
+        counter = myElement.getAttribute("counter").getIntValue();
+    }
+
+    @Override
+    public XMLElement saveSelf() throws XMLCannotSaveException {
+        XMLElement result = new XMLElement("dispenser");
+        result.addAttribute(new XMLAttribute("counter", counter));
+        return result;
+    }
+
+    @Override
+    public void saveChilds(XMLSaver saver) throws XMLTreeException, IOException, XMLCannotSaveException {
+
+        XMLArray.saveArray(new ArrayList<>(Arrays.asList(stack.toArray())), this, saver, "stack");
+    }
+
+    @Override
+    public String getXMLName() {
+        return parentName + ".dispenser";
+    }
+
+    @Override
+    public void setParentName(String parentName) {
+        this.parentName = parentName;
+    }
+
 }
